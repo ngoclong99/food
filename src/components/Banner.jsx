e2e1d82ef16c7423518ce54908c90a0c974c837b
Long <vhnlong@cmcglobal.vn>
2022-09-30 16:45:49 +0700

    Word

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux'
 import { getTheme } from '@/reducers/settingSlice'
 import { getSettingTheme } from '@/utils/config'
 import { TEXT } from '@/utils/constant'
+import { useTypingText } from './common/useTypingText'
 
 Banner.propTypes = {}
 
@@ -12,6 +13,26 @@ export function Banner(props) {
   const [banner, setBanner] = useState({})
   const [loading, setLoading] = useState(false)
   const theme = useSelector(getTheme)
+  const { word } = useTypingText(['fast', 'reliable', 'affordable'], 40, 40)
+
+  //
+
+  const [text, setText] = useState('')
+  const [fullText, setFullText] = useState(
+    'Your source of leading edge water and air treatment technology since 1994.',
+  )
+  const [index, setIndex] = useState(0)
+
+  //
+  useEffect(() => {
+    if (index < fullText.length) {
+      setTimeout(() => {
+        setText(text + fullText[index])
+        setIndex(index + 1)
+      }, 40)
+    }
+  }, [index])
+  //
 
   useEffect(() => {
     ;(async () => {
@@ -33,9 +54,11 @@ export function Banner(props) {
     <>
       <div className="banner" style={{ backgroundImage: `url(${banner.thumbnail})` }}>
         <p>
-          {banner.title} <span className={`${getSettingTheme(TEXT, theme)}`}>{banner.textTitle?.[0]}</span>
+          {banner.title}
+          <span className={`${getSettingTheme(TEXT, theme)}`}>{word}</span>
         </p>
         <p>{banner.description}</p>
+        <h2>{text}</h2>
       </div>
     </>
   )
